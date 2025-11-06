dd201f329a20744436b364b1e9f3a486398d882f
Ainur <Pet923@yandex.ru>
2025-11-06 15:41:46 +0300

    Переписал тест из первого урока используя Pytest, Сделал один новый тест на невалидный поиск

--- test_fixtures.py
+++ test_fixtures.py
@@ -1,19 +1,27 @@
 import pytest
+from selene import browser, be, have
 
 
 @pytest.fixture
-def browser():
-    pass
-
-@pytest.fixture
-def user():
-    pass
+def setup_browser():
+    browser.open('https://duckduckgo.com/')
 
 
 @pytest.fixture
-def login_page():
-    pass
+def input_text_search(setup_browser):
+    browser.element('[name="q"]').should(be.blank).type('qa.guru').press_enter()
 
 
-def test_login(user):
-    pass
+@pytest.fixture
+def input_invalid_text_search(setup_browser):
+    browser.element('[name="q"]').should(be.blank).type('cvzvzvzvzxvzxvzxvzx').press_enter()
+
+
+
+def test_find_text_to_search(input_text_search):
+    browser.element('#react-layout').should(have.text('QA.GURU'))
+    browser.element('[data-result="snippet"]').should(have.text(
+        'Освойте тестирование ПО с нуля: курсы QA на Java, Python, JS. Практика от экспертов, портфолио на GitHub, помощь в трудоустройстве.'))
+
+def test_invalid_text_to_search(input_invalid_text_search):
+    browser.element('#react-layout').should(have.text('По запросу «cvzvzvzvzxvzxvzxvzx» ничего не найдено.'))

fc969d105730443d0cd7b23e39223998cf219447
Ainur <Pet923@yandex.ru>
2025-11-06 09:26:30 +0300

    Добавил пример теста и фикстур

---
+++ test_fixtures.py
@@ -0,0 +1,19 @@
+import pytest
+
+
+@pytest.fixture
+def browser():
+    pass
+
+@pytest.fixture
+def user():
+    pass
+
+
+@pytest.fixture
+def login_page():
+    pass
+
+
+def test_login(user):
+    pass
